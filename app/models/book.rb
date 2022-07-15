5fb9b50bbdecf2a3bb6985953024282758329026
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-07-15 15:18:54 +0000

    [Update] コメント機能（アソシエーション設定）

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user) # このメソッドで、引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べる。
    favorites.where(user_id: user.id).exists?
   end
