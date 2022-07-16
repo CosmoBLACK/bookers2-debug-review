b6dced145c4083c808ffdce8f41bb571da208cc4
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-07-16 14:57:51 +0000

    [Finish] フォロー・フォロワー機能

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,4 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User" # モデル名を指定し、usersテーブルを参照する
   belongs_to :followed, class_name: "User" # モデル名を指定し、usersテーブルを参照する
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # 自分がフォローする（与フォロー）側の関係性
-  has_many :followings, throuth: :relationships, source: :followerd # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # 自分がフォローされる（被フォロー）側の関係性
-  has_many :followers, throuth: :reverse_of_relationships, source: :follower # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower # 被フォロー関係を通じて参照→自分をフォローしている人
 
   has_one_attached :profile_image
 
