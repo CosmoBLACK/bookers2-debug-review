ac248567bddb41f0a31445f7bc348a42ab68a6ad
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-07-17 12:11:22 +0000

    [Update] いいね機能非同期化完了、コメント機能非同期化中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id]) # 本の投稿から”ある投稿”を取り出す。findメソッドでデータベースから取り出して、bookというローカル変数に入れる。
-    comment = current_user.book_comments.new(book_comment_params) # コメントを新規投稿するための空のインスタンスを作る。
-    # comment = BookComment.new(book_comment_params)、comment.user_id = current_user.idと同じ意味
-    comment.book_id = book.id # 空のインスタンスのbook_idは、今取り出してる"ある投稿”のidを意味する。
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id]) # 本の投稿から”ある投稿”を取り出す。findメソッドでデータベースから取り出して、@bookに入れる。
+    @book_comment = current_user.book_comments.new(book_comment_params) # コメントを新規投稿するための空のインスタンスを作る。
+    # @book_comment = BookComment.new(book_comment_params)、@book_comment.user_id = current_user.idと同じ意味
+    @book_comment.book_id = @book.id # 空のインスタンスのbook_idは、今取り出してる"ある投稿”のidを意味する。
+    unless @book_comment.save
+      render 'error'
+    end
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
   end
 
   private
